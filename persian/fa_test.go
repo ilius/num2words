a3b3d9176e7888930cbe4dbd0d46fe67427d5bb8
Saeed Rasooli <saeed.gnu@gmail.com>
2024-03-28 14:12:16 +0330

    fa_test.go: change testData to []TestCase and fix benchmark

--- persian/fa_test.go
+++ persian/fa_test.go
@@ -15,7 +15,13 @@ import (
 
 var testData = loadTestData()
 
-func loadTestData() map[string]string {
+type TestCase struct {
+	String string
+	BigInt *big.Int
+	Words  string
+}
+
+func loadTestData() []TestCase {
 	file, err := os.Open("test-data.gz")
 	if err != nil {
 		panic(err)
@@ -26,7 +32,7 @@ func loadTestData() map[string]string {
 		panic(err)
 	}
 	scanner := bufio.NewScanner(zfile)
-	data := map[string]string{}
+	data := []TestCase{}
 	for scanner.Scan() {
 		line := scanner.Text()
 		parts := strings.SplitN(line, "\t", 2)
@@ -34,8 +40,14 @@ func loadTestData() map[string]string {
 			log.Fatalf("bad line: %v", line)
 		}
 		num_str := parts[0]
-		words_expected := parts[1]
-		data[num_str] = words_expected
+		words := parts[1]
+		bn := &big.Int{}
+		bn.SetString(num_str, 10)
+		data = append(data, TestCase{
+			String: num_str,
+			BigInt: bn,
+			Words:  words,
+		})
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
@@ -44,15 +56,15 @@ func loadTestData() map[string]string {
 }
 
 func TestConvertString(t *testing.T) {
-	for num_str, words_expected := range testData {
-		words, err := persian.ConvertString(num_str)
+	for _, tc := range testData {
+		words, err := persian.ConvertString(tc.String)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if words != words_expected {
+		if words != tc.Words {
 			log.Fatalf(
 				"test failed: num=%v, expected: %#v, actual: %#v",
-				num_str, words_expected, words,
+				tc.String, tc.Words, words,
 			)
 		}
 	}
@@ -60,52 +72,50 @@ func TestConvertString(t *testing.T) {
 
 func TestConvertBigInt(t *testing.T) {
 	is := is.New(t)
-	for num_str, words_expected := range testData {
-		bn := &big.Int{}
-		bn.SetString(num_str, 10)
-		is.Equal(persian.ConvertBigInt(bn), words_expected)
+	for _, tc := range testData {
+		is.Equal(persian.ConvertBigInt(tc.BigInt), tc.Words)
 	}
 }
 
+var big_zero = big.NewInt(0)
+
 func TestConvertBigIntSigned(t *testing.T) {
 	is := is.New(t)
-	for num_str, words_expected := range testData {
+	for _, tc := range testData {
 		bn_neg := &big.Int{}
-		bn_neg.SetString(num_str, 10)
+		bn_neg.SetBytes(tc.BigInt.Bytes())
 		bn_neg.Neg(bn_neg)
-		if num_str == "0" {
+		if tc.BigInt.Cmp(big_zero) == 0 {
 			is.Equal(persian.ConvertBigIntSigned(bn_neg), "صفر")
 		} else {
-			is.Equal(persian.ConvertBigIntSigned(bn_neg), "منفی "+words_expected)
+			is.Equal(persian.ConvertBigIntSigned(bn_neg), "منفی "+tc.Words)
 		}
 	}
 }
 
 func Benchmark_convert_string_bigInt(b *testing.B) {
 	b.Run("string", func(b *testing.B) {
-		for num_str := range testData {
-			persian.ConvertString(num_str)
+		for _, tc := range testData {
+			persian.ConvertString(tc.String)
 		}
 	})
 	b.Run("big.Int", func(b *testing.B) {
-		for num_str := range testData {
-			bn := &big.Int{}
-			bn.SetString(num_str, 10)
-			persian.ConvertBigInt(bn)
+		for _, tc := range testData {
+			persian.ConvertBigInt(tc.BigInt)
 		}
 	})
 }
 
 /*
 go test -bench=. -benchtime=1000x -count=5
-	Benchmark_convert_string_bigInt/string-8         	    1000	      3500 ns/op
-	Benchmark_convert_string_bigInt/string-8         	    1000	      3388 ns/op
-	Benchmark_convert_string_bigInt/string-8         	    1000	      3383 ns/op
-	Benchmark_convert_string_bigInt/string-8         	    1000	      3769 ns/op
-	Benchmark_convert_string_bigInt/string-8         	    1000	      3614 ns/op
-	Benchmark_convert_string_bigInt/big.Int-8        	    1000	      6050 ns/op
-	Benchmark_convert_string_bigInt/big.Int-8        	    1000	      6301 ns/op
-	Benchmark_convert_string_bigInt/big.Int-8        	    1000	      7827 ns/op
-	Benchmark_convert_string_bigInt/big.Int-8        	    1000	      6571 ns/op
-	Benchmark_convert_string_bigInt/big.Int-8        	    1000	      6183 ns/op
+	Benchmark_convert_string_bigInt/string-8         	    1000	      3152 ns/op
+	Benchmark_convert_string_bigInt/string-8         	    1000	      3158 ns/op
+	Benchmark_convert_string_bigInt/string-8         	    1000	      3205 ns/op
+	Benchmark_convert_string_bigInt/string-8         	    1000	      3135 ns/op
+	Benchmark_convert_string_bigInt/string-8         	    1000	      3954 ns/op
+	Benchmark_convert_string_bigInt/big.Int-8        	    1000	      5158 ns/op
+	Benchmark_convert_string_bigInt/big.Int-8        	    1000	      5272 ns/op
+	Benchmark_convert_string_bigInt/big.Int-8        	    1000	      5864 ns/op
+	Benchmark_convert_string_bigInt/big.Int-8        	    1000	      5045 ns/op
+	Benchmark_convert_string_bigInt/big.Int-8        	    1000	      5496 ns/op
 */
